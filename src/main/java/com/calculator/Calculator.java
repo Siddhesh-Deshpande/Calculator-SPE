706224c29b65657b0e2491c3fe89cfd2c409cfa4
Siddhesh Deshpande <Siddhesh.Deshpande@iiitb.ac.in>
2025-11-12 22:41:55 +0530

    Testing before viva 3

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -73,7 +73,7 @@ public class Calculator
             {
                 System.out.println("Invalid Option, Enter Correct Option");
             }
-            // Checking Before VIVA-2
+            // Checking Before VIVA-3
         }
     }
     public static double findSquareRoot(double number)
